03b6071ccbf2cbeffc864199a4b6e8f0f081ed94
Mantton <mantton.official@gmail.com>
2023-03-29 22:07:08 -0400

    feat: revamp add manga search panel

--- src/components/AnilistMangaSearch.tsx
+++ src/components/AnilistMangaSearch.tsx
@@ -1,9 +1,16 @@
 import { type AnilistSearchMedia } from "@/types";
 import { searchAnilist } from "@/utils/anilist";
-import { useState, useEffect, useContext, createContext } from "react";
+import {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  Fragment,
+} from "react";
 import { LoadingSpinner } from "./loading";
 import { AiOutlinePlusCircle, AiOutlineCloseCircle } from "react-icons/ai";
 import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
 
 /**
  * Selection Context
@@ -119,7 +126,7 @@ export default function AnilistMangaSearch() {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState<AnilistSearchMedia[]>([]);
-  const { selections } = useSelectionContext();
+  const { selections, insertMedia } = useSelectionContext();
   useEffect(() => {
     const debounce = setTimeout(() => {
       const run = async () => {
@@ -148,33 +155,90 @@ export default function AnilistMangaSearch() {
     <>
       <div className="flex w-3/4 flex-col gap-4 rounded-md bg-gray-50 py-2">
         <p className="px-2 text-lg font-medium">Add Manga</p>
-        <div className="mx-auto w-[90%]">
-          <div className="relative flex h-12 w-full items-center overflow-hidden rounded-lg bg-white focus-within:shadow-lg">
-            {!loading && <SearchIcon />}
-            {loading && (
-              <div className="p-2">
-                <LoadingSpinner />
+        <div className="relative mx-auto w-[90%]">
+          <Combobox
+            onChange={(v) => {
+              insertMedia(v as AnilistSearchMedia);
+            }}
+          >
+            <div className="relative flex h-12 w-full items-center overflow-hidden rounded-lg bg-white focus-within:shadow-lg">
+              <div className="">
+                {!loading && <SearchIcon />}
+                {loading && (
+                  <div className="p-2">
+                    <LoadingSpinner />
+                  </div>
+                )}
               </div>
-            )}
-            <input
-              className="peer h-full w-full pr-2 text-sm text-gray-700 outline-none"
-              id="search"
-              placeholder="Search Anilist"
-              autoComplete="off"
-              onChange={(e) => setQuery(e.target.value.trim())}
-              onKeyPress={(e) => {
-                e.key === "Enter" && e.preventDefault();
-              }}
-            />
-          </div>
+              <Combobox.Input
+                className="h-full w-full border-none pr-2 text-sm text-gray-700"
+                onChange={(event) => setQuery(event.target.value.trim())}
+                id="search"
+                placeholder="Search Anilist"
+                // autoComplete="off"
+                // onKeyPress={(e) => {
+                //   e.key === "Enter" && e.preventDefault();
+                // }}
+              />
+            </div>
+            <Transition
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              afterLeave={() => setQuery("")}
+            >
+              <Combobox.Options className="absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded-md bg-white p-2 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+                {results.map((manga) => (
+                  <Combobox.Option key={manga.id} value={manga}>
+                    <div className="flex cursor-pointer gap-4 rounded-md p-4 transition-colors duration-200 hover:bg-slate-100">
+                      <Image
+                        src={manga.coverImage.large}
+                        alt={manga.title.userPreferred}
+                        width={55}
+                        height={82.5}
+                        className="rounded-md"
+                      />
+                      <div className="flex flex-col">
+                        <p className="font-medium">
+                          {manga.title.userPreferred}
+                        </p>
+                        {manga.title.english &&
+                          manga.title.english != manga.title.userPreferred && (
+                            <p className="text-sm italic text-slate-700">
+                              {manga.title.english}
+                            </p>
+                          )}
+                        <div className="flex flex-wrap items-center gap-2">
+                          {manga.isAdult && (
+                            <div className="w-fit rounded-md bg-red-100 px-1 py-1 text-slate-500">
+                              <p className="text-xs leading-tight tracking-tighter text-opacity-95">
+                                NSFW
+                              </p>
+                            </div>
+                          )}
+                          {manga.genres[0] && (
+                            <div className="flex gap-2 ">
+                              {manga.genres.map((v) => (
+                                <span
+                                  className="w-fit rounded-md bg-slate-100 px-2 py-1 text-xs text-slate-500"
+                                  key={v}
+                                >
+                                  {v}
+                                </span>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            </Transition>
+          </Combobox>
         </div>
 
-        <div>
-          {results[0] && <MediaList media={results} addition={true} />}
-          <div hidden={!!results[0]}>
-            {selections[0] && <MediaList media={selections} addition={false} />}
-          </div>
-        </div>
+        {selections[0] && <MediaList media={selections} addition={false} />}
       </div>
     </>
   );
